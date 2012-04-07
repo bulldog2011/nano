273c7d7d355566cbcf8fe2e5cffeabada23764d1
bulldog <51startup@sina.com>
2012-04-08 02:38:34 +0800

    1. added Base64TransformTest 2. removed eclipse project file from source control

---
+++ src/test/java/com/tpt/nano/transform/Base64TransformTest.java
@@ -0,0 +1,20 @@
+package com.tpt.nano.transform;
+
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+public class Base64TransformTest extends TestCase {
+	
+	public void testBase64() throws Exception {
+		byte[] data = "hello world!".getBytes();
+		Base64Transform transform = new Base64Transform();
+		
+		String value = transform.write(data);
+		byte[] copy = transform.read(value);
+		System.out.println(new String(copy));
+
+		assertTrue(Arrays.equals(data, copy));
+	}
+
+}
